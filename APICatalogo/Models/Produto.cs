fa6e41d98c8f1c7f48a0a50422697e591fe51b6e
Heitor Fidelis <heitorfidelis81@gmail.com>
2025-05-20 01:01:52 -0400

    Validação Personalizada

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using APICatalogo.Validations;
 
 namespace APICatalogo.Models;
 
@@ -8,8 +9,9 @@ public class Produto
 {
     public int ProdutoId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage ="O nome é obrigatório")]
     [StringLength(80)]
+    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
