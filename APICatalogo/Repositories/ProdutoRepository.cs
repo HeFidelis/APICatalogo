efac493139179e4bc507c1ea368133ee26c1bbc1
Heitor Fidelis <heitorfidelis81@gmail.com>
2025-05-27 01:55:09 -0400

    Implementando um Filtro para Produtos

--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -1,4 +1,4 @@
-﻿    using APICatalogo.Context;
+﻿using APICatalogo.Context;
 using APICatalogo.Models;
 using APICatalogo.Pagination;
 
@@ -17,6 +17,31 @@ public class ProdutoRepository : Repository<Produto>, IProdutoRepository
         var produtosOrdenados = PagedList<Produto>.ToPagedList(produtos, produtosParams.PageNumber, produtosParams.PageSize);
 
         return produtosOrdenados;
+    }
+
+    public PagedList<Produto> GetProdutosFiltroPreco(ProdutosFiltroPreco produtosFiltroParams)
+    {
+        var produtos = GetAll().AsQueryable();
+
+        if (produtosFiltroParams.Preco.HasValue && !string.IsNullOrEmpty(produtosFiltroParams.PrecoCriterio))
+        {
+            if (produtosFiltroParams.PrecoCriterio.Equals("maior", StringComparison.OrdinalIgnoreCase))
+            {
+                produtos = produtos.Where(p => p.Preco > produtosFiltroParams.Preco.Value).OrderBy(p => p.Preco);
+            }
+            else if (produtosFiltroParams.PrecoCriterio.Equals("menor", StringComparison.OrdinalIgnoreCase))
+            {
+                produtos = produtos.Where(p => p.Preco < produtosFiltroParams.Preco.Value).OrderBy(p => p.Preco);
+            }
+            else if (produtosFiltroParams.PrecoCriterio.Equals("igual", StringComparison.OrdinalIgnoreCase))
+            {
+                produtos = produtos.Where(p => p.Preco == produtosFiltroParams.Preco.Value).OrderBy(p => p.Preco);
+            }
+        }
+
+        var produtosFiltrados = PagedList<Produto>.ToPagedList(produtos, produtosFiltroParams.PageNumber, 
+                                                                                        produtosFiltroParams.PageSize);
+        return produtosFiltrados;
     }
 
     public IEnumerable<Produto> GetProdutosPorCategoria(int id)
