77891371e290e988961c32269018ecd439288fa8
Heitor Fidelis <heitorfidelis81@gmail.com>
2025-05-26 21:57:37 -0400

    Implementando a paginação

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,11 +1,13 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.DTOs;
 using APICatalogo.Models;
+using APICatalogo.Pagination;
 using APICatalogo.Repositories;
 using AutoMapper;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace APICatalogo.Controllers;
 
@@ -33,6 +35,29 @@ public class ProdutosController : ControllerBase
         }
 
         //var destino = _mapper.Map<Destino>(origem);
+        var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
+
+        return Ok(produtosDto);
+    }
+
+    [HttpGet("pagination")]
+    public ActionResult<IEnumerable<ProdutoDTO>> Get([FromQuery] 
+                                   ProdutosParameters produtosParameters)
+    {
+        var produtos = _uof.ProdutoRepository.GetProdutos(produtosParameters);
+
+        var metadata = new
+        {
+            produtos.TotalCount,
+            produtos.PageSize,
+            produtos.CurrentPage,
+            produtos.TotalPages,
+            produtos.HasNext,
+            produtos.HasPrevious
+        };
+
+        Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
+
         var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
 
         return Ok(produtosDto);
--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -1,5 +1,6 @@
-﻿using APICatalogo.Context;
+﻿    using APICatalogo.Context;
 using APICatalogo.Models;
+using APICatalogo.Pagination;
 
 namespace APICatalogo.Repositories;
 
@@ -7,6 +8,13 @@ public class ProdutoRepository : Repository<Produto>, IProdutoRepository
 {
     public ProdutoRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public PagedList<Produto> GetProdutos(ProdutosParameters produtosParams)
+    {
+        var produtos = GetAll().OrderBy(p => p.ProdutoId).AsQueryable();
+        var produtosOrdenados = PagedList<Produto>.ToPagedList(produtos, produtosParams.PageNumber, produtosParams.PageSize);
+        return produtosOrdenados;
     }
 
     public IEnumerable<Produto> GetProdutosPorCategoria(int id)
