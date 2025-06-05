9326a7ae01c5d29b2c1c7223779d21010e5feb29
Heitor Fidelis <heitorfidelis81@gmail.com>
2025-06-04 23:24:07 -0400

    Configurando as políticas de autorização

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -93,6 +93,22 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+
+    options.AddPolicy("SuperAdminOnly", policy =>
+                        policy.RequireRole("Admin").RequireClaim("id", "heitor"));
+
+    options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+
+    options.AddPolicy("ExclusivePolicyOnly", policy =>
+                       policy.RequireAssertion(context =>
+                       context.User.HasClaim(claim =>
+                       claim.Type == "id" && claim.Value == "heitor") 
+                       || context.User.IsInRole("SuperAdmin")));
+});
+
 builder.Services.AddScoped<ApiLoggingFilter>();
 builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
