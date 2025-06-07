3c0163199c3cc1a58f13d67597916e854c899e56
Heitor Fidelis <heitorfidelis81@gmail.com>
2025-06-06 22:06:25 -0400

    Criando as classes para realizar os testes de unidade

---
+++ ApiCatalogoxUnitTests/UnitTests/GetProdutoUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using APICatalogo.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoxUnitTests.UnitTests;
+
+public class GetProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
+{
+    private readonly ProdutosController _controller;
+
+    public GetProdutoUnitTests(ProdutosUnitTestController controller)
+    {
+        _controller = new ProdutosController(controller.repository, controller.mapper);
+    }
+
+    [Fact]
+    public async Task GetProdutoById_OkResult()
+    {
+        //Arrange
+        var prodId = 2;
+
+        //Act
+        var data = await _controller.Get(prodId);
+
+        //Assert (xUnit)
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert (FluentAssertions)
+        data.Result.Should().BeOfType<OkObjectResult>() //verifica se o resultado é do tipo OkObjectResult
+                   .Which.StatusCode.Should().Be(200); //verifica se o status code é 200
+    }
+}
